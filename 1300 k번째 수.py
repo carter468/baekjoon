f459252e42ee2f9301bf4bdbf5ea6a8ee86a269e
DaeGeon Kim <kdg870918.gmail.com>
2022-07-04 13:33:07 +0900

    220704

--- 1300 k번째 수.py
+++ 1300 k번째 수.py
@@ -2,4 +2,22 @@
 
 n = 3
 k = 7
+n = int(input())
+k = int(input())
 
+start = 1
+end = k
+while start<end:
+    mid = (start+end)//2
+
+    cnt = 0
+    for i in range(1,n+1):
+        cnt += min(mid//i,n)
+    print(mid,cnt)
+    input()
+    if cnt < k:
+        start = mid+1
+    else:
+        end = mid
+
+print(start)
