01153742a115cdb57730bb0b9e1543e92c53e5ed
DaeGeon Kim <kdg870918.gmail.com>
2022-07-19 13:00:29 +0900

    220719

---
+++ 1956 운동.py
@@ -0,0 +1,27 @@
+# 운동
+import sys
+input = sys.stdin.readline
+
+v,e = map(int,input().split())
+
+inf = int(10**9)
+graph = [[inf]*(v) for _ in range(v)]
+
+for _ in range(e):
+    a,b,c = map(int,input().split())
+    graph[a-1][b-1] = c
+
+for k in range(v):  # 거쳐가는 정점
+    for i in range(v):  
+        for j in range(v):
+            graph[i][j] = min(graph[i][j],graph[i][k] + graph[k][j])
+
+ans = inf
+
+for i in range(v):
+    ans = min(ans,graph[i][i])
+
+if ans == inf:
+    print(-1)
+else:
+    print(ans)
