cc3e22a27cbd7435801de7dbf34ff7830080810a
DaeGeon Kim <kdg870918.gmail.com>
2022-10-14 07:20:55 +0900

    221014

--- 18353 병사배치하기.py
+++ 18353 병사배치하기.py
@@ -2,12 +2,9 @@
 
 n = int(input())
 arr = list(map(int,input().split()))
-
-dp = [0]*n
-arr_len = 0
+dp = [1]*n
 for i in range(n):
-    dp[i] = 1
     for j in range(i):
-        if arr[j] > arr[i] and dp[j]+1 > dp[i]:
+        if arr[j] > arr[i] and dp[j] + 1 > dp[i]:
             dp[i] = dp[j] + 1
 print(n - max(dp))
