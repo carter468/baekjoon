9443eedfd0f82e8c399a12caaf84177b58860830
DaeGeon Kim <kdg870918.gmail.com>
2022-10-14 15:09:32 +0900

    221014

--- 11375 열혈강호.py
+++ 11375 열혈강호.py
@@ -15,12 +15,12 @@ def dfs(node):
         return False
     visit[node] = 1
     for next in graph[node]:
-        if matching[next] == 0:
+        if matching[next] == 0:     # 배정된 직원이 없다
             matching[next] = node
             return True
         else:
             prev = matching[next]
-            if dfs(prev):
+            if dfs(prev):           # 이미 배정된 직원이 다른 일에 배정가능한경우
                 matching[next] = node
                 return True
     return False
@@ -28,10 +28,4 @@ def dfs(node):
 for i in range(1,n+1):
     visit = [0 for _ in range(n+1)]
     dfs(i)
-ans = 0
-for i in range(m+1):
-    if matching[i]>0:
-        ans += 1
-print(matching)
-print(ans)
-print(m-matching.count(0)+1)
+print(m+1-matching.count(0))
---
+++ 1708 볼록껍질.py
@@ -0,0 +1,12 @@
+# 볼록껍질
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+answer = -2
+coordinate = []
+for _ in range(n):
+    coordinate.append(list(map(int,input().split())))
+coordinate.sort(key=lambda x:(x[0],x[1]))
+answer += convexhull()
