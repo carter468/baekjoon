eb6af8faba2a6b1a18fed614822ede654a14815b
DaeGeon Kim <kdg870918.gmail.com>
2022-11-04 13:14:03 +0900

    221104

--- 1725 히스토그램.py
+++ 1725 히스토그램.py
@@ -4,29 +4,18 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-height = [0]*(n+1)
-for i in range(n):
-    height[i] = int(input())
+height = []
+for _ in range(n):
+    height.append(int(input()))
+height.append(0)
 
 answer = 0
-stack = []
-for i in range(n):
-    left = i
+stack = [-1]
+for i in range(n+1):
     while stack and height[stack[-1]] > height[i]:
-        h = height[stack[-1]]
-        stack.pop()
-        w = i
-        if stack:
-            w -= stack[-1]+1
+        h = height[stack.pop()]
+        w = i-stack[-1]-1
         answer = max(answer,h*w)
     stack.append(i)
-
-while stack:
-    h = height[stack[-1]]
-    stack.pop()
-    w = n
-    if stack:
-        w -= stack[-1]+1
-    answer = max(answer,h*w)
 
 print(answer)
