a07d480ffcefbeb2b3e0ed822622373094eb900e
DaeGeon Kim <kdg870918.gmail.com>
2022-11-05 10:12:43 +0900

    221105

--- 11780 플로이드2.py
+++ 11780 플로이드2.py
@@ -2,13 +2,38 @@
 
 import sys
 input = sys.stdin.readline
+INF = sys.maxsize
 
-n = int(input())    # 도시의 개수
-m = int(input())    # 버스의 개수
+def find_path(i,j):
+    if trace[i][j] == 0:
+        return []
+    k = trace[i][j]
+    return find_path(i,k)+[k]+find_path(k,j)
 
-graph = [[]*(n+1) for _ in range(n+1)]
-for _ in range(m):
+n = int(input())   
+distance = [[INF]*(n+1) for _ in range(n+1)]
+for i in range(1,n+1):
+    distance[i][i] = 0
+for _ in range(int(input())):
     a,b,c = map(int,input().split())
-    graph[a].append([b,c])
+    distance[a][b] = min(distance[a][b],c)
 
-print(graph)
+trace = [[0]*(n+1) for _ in range(n+1)]
+for k in range(1,n+1):
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            if distance[i][j] > distance[i][k] + distance[k][j]:
+                distance[i][j] = distance[i][k] + distance[k][j]
+                trace[i][j] = k
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(distance[i][j] if distance[i][j]!=INF else 0,end=' ')
+    print()
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if distance[i][j]==0 or distance[i][j]==INF:
+            print(0)
+            continue
+        path = [i]+find_path(i,j)+[j]
+        print(len(path),*path)
