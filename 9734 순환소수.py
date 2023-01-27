4032e1814d5fdee26e9de4a02a49dcc013ee4a1f
DaeGeon Kim <kdg870918.gmail.com>
2023-01-27 10:25:42 +0900

    230127

--- 9734 순환소수.py
+++ 9734 순환소수.py
@@ -1,25 +1,22 @@
 # 순환소수
+# Silver 1, 유클리드 호제법, 파싱, 수학
+
 import sys
+
+def GCD(m,n):
+    while n!=0:
+        m,n = n,m%n
+    return m
 
 while True:
     try:
-        x = sys.stdin.readline().strip()
-        a,e = x.split('.')   
-        a = int(a)  # 정수부분
-        b,c = e[:-1].split('(')    # 순환하지 않는 부분, 순환하는 부분
-        d = int(b) if b else 0     # 순환하지 않는 부분
-        f = int('9'*len(c)+'0'*len(b))  # 분모
-        g = int(b+c)-d # 분자
+        a = sys.stdin.readline().strip()
+        x,b = a.split('.')
+        y,z = b[:-1].split('(')
+        p = int('9'*len(z)+'0'*len(y))
+        q = int(x+y+z)-int(x+y)
+        g = GCD(p,q)
+        print(f'{a} = {q//g} / {p//g}')
 
-        # 유클리드 호제법
-        h,i = f,g
-        while i!=0:
-            h,i = i,h%i
-
-        # 기약분수
-        f //= h
-        g //= h
-        g += a * f
-        print(f'{x} = {g} / {f}')
-    except EOFError:
+    except:
         break
