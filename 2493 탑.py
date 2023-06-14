91410b62cff11b020fafdcf3c2317aa2b1332de7
DaeGeon Kim <kdg870918@gmail.com>
2023-06-14 10:08:02 +0900

    230614

--- 2493 탑.py
+++ 2493 탑.py
@@ -13,3 +13,13 @@ for i in range(n):
     else:
         print(0,end=' ')
     tower.append((arr[i],i+1))
+
+# n = int(input())
+
+# arr = tuple(map(int,input().split()))
+# tower = [(10**8,0)]
+# for i in range(n):
+#     while tower[-1][0] < arr[i]:
+#         tower.pop()
+#     print(tower[-1][1],end=' ')
+#     tower.append((arr[i],i+1))
