bc9ffc88adaeb560bad4f4a25bc1e21e130b945f
DaeGeon Kim <kdg870918@gmail.com>
2023-07-23 10:04:42 +0900

    230723

---
+++ 17268 미팅의저주.py
@@ -0,0 +1,13 @@
+# 미팅의 저주
+# Gold 3, DP
+
+n = int(input())//2
+
+dp = [0]*(n+1)
+dp[0],dp[1] = 1,1
+for i in range(2,n+1):
+    for j in range(i):
+        dp[i] += dp[j]*dp[i-1-j]%987654321
+    dp[i] %= 987654321
+    
+print(dp[n])
