8cd5da5d6a7acf801ac4e231ca87afa6bfc62812
DaeGeon Kim <kdg870918@gmail.com>
2024-05-25 08:34:44 +0900

    240525

---
+++ 15482 한글LCS.py
@@ -0,0 +1,14 @@
+# 한글 LCS
+# Gold 5, DP
+
+s = input()
+t = input()
+
+dp = [[0]*(len(t)+1) for _ in range(len(s)+1)]
+for i in range(len(s)):
+    for j in range(len(t)):
+        if s[i] == t[j]:
+            dp[i+1][j+1] = dp[i][j]+1
+        else:
+            dp[i+1][j+1] = max(dp[i+1][j],dp[i][j+1])
+print(dp[-1][-1])
