6a5fb2cc142b5eb54a25550bc08b72234e9906a9
DaeGeon Kim <kdg870918@gmail.com>
2025-08-29 07:32:53 +0900

    250829

---
+++ 10090 CountingInversions.py
@@ -0,0 +1,24 @@
+# Counting Inversions
+# Platinum 5, fenwick tree
+
+def prefix_sum(i):
+    result = 0
+    while i > 0:
+        result += tree[i]
+        i -= (i&-i)
+    return result
+
+def update(i):
+    while i <= N:
+        tree[i] += 1
+        i += (i&-i)
+
+N = int(input())
+A = tuple(map(int,input().split()))
+
+tree = [0]*(N+1)
+count = 0
+for i in range(N):
+    count += i-prefix_sum(A[i])
+    update(A[i])
+print(count)
